6d854e545a4efb3b9935d496e28057eff7a76822
Lucas Marques Garrido <95510208+LucasMGarrido@users.noreply.github.com>
2022-06-20 20:12:42 -0300

    CBB Exercício 1

---
+++ CBBAtividadePratica01.java
@@ -0,0 +1,11 @@
+class CBBAtividadePratica01 {
+  public static void main(String[] args) {
+    String frase1;
+    String frase2;
+    frase1 = new String("Testando igualdade!");
+    frase2 = new String("Testando igualdade!");
+    System.out.println("\nFrase 1: " +frase1);
+    System.out.println("\nFrase 2: " +frase2);
+    System.out.println("\nFrase 1 e Frase 2 são iguais? \nResultado: " +(frase1==frase2));
+  }
+}
